4ec9587e3c3c55a65fe5b0b0ff55866a069f4a04
rekha.singh <rekha.r.singh@accenture.com>
2023-04-14 12:43:34 -0500

    upto module 3

--- src/app.js
+++ src/app.js
@@ -8,8 +8,25 @@ app.set('views',path.join(__dirname,'views'));
 app.set("view engine","ejs");
 app.use(express.static(path.join(__dirname,'public')));
 
+const accountData=fs.readFileSync(path.join(__dirname,'json/accounts.json'));
+const accounts=JSON.parse(accountData);
+
+const userData=fs.readFileSync(path.join(__dirname,'json/users.json'));
+const users=JSON.parse(accountData);
 ap.get("/",function(req,res){
-    res.render('index',{title:'Index'});
+    res.render('index',{title:'Account Summary',accounts:accounts});
+});
+ap.get("/savings",function(req,res){
+    res.render('account',{account:accounts.savings});
+});
+ap.get("/checking",function(req,res){
+    res.render('account',{account:accounts.checking});
+});
+ap.get("/credit",function(req,res){
+    res.render('account',{account:accounts.credit});
+});
+ap.get("/profile",function(req,res){
+    res.render('profile',{user:users[0]});
 });
 
 app.listen(3000,()=>{
